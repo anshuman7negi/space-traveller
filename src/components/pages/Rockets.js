4b898cfa8f1327398b25f8ff92158b531105de52
anshuman7negi <anshumannegi777@gmail.com>
2023-08-28 17:53:11 +0530

    Created basic structure of rocket

--- src/components/pages/Rockets.js
+++ src/components/pages/Rockets.js
@@ -1,5 +1,21 @@
-const Rockets = () => (
-  <div />
-);
+import rocket from '../../assets/rocket.jpg';
+import '../../style/Rocket.css';
 
+const Rockets = () => (
+  <div className="RocketContainer">
+    <div className="RocketList">
+      <img src={rocket} alt="" />
+      <div className="RocketDetails">
+        <h3>Falcon 1</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Officia ullam culpa libero ducimus nobis in,
+          debitis maxime ab error quae.
+          Voluptas, alias possimus numquam odio unde provident explicabo qui totam accusamus.
+        </p>
+        <button type="button">Reserve Rocket</button>
+      </div>
+    </div>
+  </div>
+);
 export default Rockets;
