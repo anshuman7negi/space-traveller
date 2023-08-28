e784649a9911734cfa01e53b482431fa7a178610
Lord Zodd <hamzatbolaji648@yahoo.com>
2023-08-28 17:00:55 +0100

    Basic Setup for Mission

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,21 +1,21 @@
 const Missions = () => {
-    return(
-      <div className="px-[5rem] py-[2rem] font-manrope">
-        <table className="border text-left">
-          <thead className="h-16">
-            <tr>
-              <th className="border pl-5 w-1/6">Mission</th>
-              <th className="border pl-5 w-3/6">Description</th>
-              <th className="border pl-5 w-1/6">Status</th>
-              <th className="border pl-5 w-1/6" aria-hidden="true" />
-            </tr>
-          </thead>
-          <tbody>
-            Happy coding!
-          </tbody>
-        </table>
-      </div>
-    )
-}
+  return (
+    <div className="px-[5rem] py-[2rem] font-manrope">
+      <table className="border text-left">
+        <thead className="h-16">
+          <tr>
+            <th className="border pl-5 w-1/6">Mission</th>
+            <th className="border pl-5 w-3/6">Description</th>
+            <th className="border pl-5 w-1/6">Status</th>
+            <th className="border pl-5 w-1/6" aria-hidden="true" />
+          </tr>
+        </thead>
+        <tbody>
+          Happy coding!
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Missions;
