70e799effb43e550e1bfc5d1f2fdd7fcc940633c
Toyyib Bolaji <58138876+Simpleshaikh1@users.noreply.github.com>
2023-09-01 07:56:28 +0100

    Revert "Added test cases for rocket and navbar"

--- src/_test_/Mission.test.js
+++ src/_test_/Mission.test.js
@@ -1,29 +1,52 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
-import store from '../redux/store';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // For the "toBeInTheDocument" matcher
 import Mission from '../components/Mission';
 
-test('Mission component renders correctly', () => {
-  const missionProps = {
-    id: 'mission-123',
+describe('Mission component', () => {
+  const mockMission = {
+    id: '1',
     missionName: 'Sample Mission',
-    description: 'A sample mission description.',
+    description: 'Sample description',
     highlights: true,
-    reserved: false,
+    reserved: true,
   };
 
-  const tree = renderer.create(
-    <Provider store={store}>
-      <Mission
-        id={missionProps.id}
-        missionName={missionProps.missionName}
-        description={missionProps.description}
-        highlights={missionProps.highlights}
-        reserved={missionProps.reserved}
-      />
-    </Provider>,
-  ).toJSON();
+  it('renders mission details correctly', () => {
+    const { getByText } = render(<Mission
+      id={mockMission.id}
+      missionName={mockMission.missionName}
+      description={mockMission.description}
+      reserved={mockMission.reserved}
+    />);
 
-  expect(tree).toMatchSnapshot();
+    // Assert that mission name, description, and reservation status are rendered
+    expect(getByText('Sample Mission')).toBeInTheDocument();
+    expect(getByText('Sample description')).toBeInTheDocument();
+    expect(getByText('Active Member')).toBeInTheDocument();
+  });
+
+  it('renders "Active Member" status when reserved is true', () => {
+    const { getByText } = render(<Mission
+      id={mockMission.id}
+      missionName={mockMission.missionName}
+      description={mockMission.description}
+      reserved={mockMission.true}
+    />);
+
+    // Assert that "Active Member" status is rendered when reserved is true
+    expect(getByText('Active Member')).toBeInTheDocument();
+  });
+
+  it('renders "NOT A MEMBER" status when reserved is false', () => {
+    const { getByText } = render(<Mission
+      id={mockMission.id}
+      missionName={mockMission.missionName}
+      description={mockMission.description}
+      reserved={mockMission.false}
+    />);
+
+    // Assert that "NOT A MEMBER" status is rendered when reserved is false
+    expect(getByText('NOT A MEMBER')).toBeInTheDocument();
+  });
 });
